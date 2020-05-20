dcb68f69dac7dff5900593115ddd243dd12f9303
0GiS0 <gisela.torres@returngis.net>
2020-05-20 07:56:38 +0200

    added more info when the app is listening

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, () => console.log('Webhook is listening on port 3000'));
+var server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Webhook sender is listening at https://${server.address().address}:${server.address().port}`);
+});
